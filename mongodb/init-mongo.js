1101edeb42a3c0ecc5af4b6e2ab1a52831f6b55c
박기현 <kihyun1998@penta.co.kr>
2024-12-26 17:54:44 +0900

    update: update db 구조

--- mongodb/init-mongo.js
+++ mongodb/init-mongo.js
@@ -1,8 +1,16 @@
 // MongoDB 초기 설정
 db = db.getSiblingDB("prisma_market");
 
-// auth_data 컬렉션 생성
+// auth_data 컬렉션 설정 유지
 db.createCollection("auth_data");
-
-// 유니크 인덱스 생성
 db.auth_data.createIndex({ email: 1 }, { unique: true });
+
+// users 컬렉션 생성 및 인덱스 설정
+db.createCollection("users");
+db.users.createIndex({ auth_id: 1 }, { unique: true });
+db.users.createIndex({ email: 1 }, { unique: true });
+db.users.createIndex({ username: 1 }, { unique: true });
+db.users.createIndex(
+  { username: "text", first_name: "text", last_name: "text" },
+  { weights: { username: 3, first_name: 2, last_name: 1 } }
+);
